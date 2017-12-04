5bb4a4033387cf08e98e121c93cc1071f0aedff7
Daniel <dnuneswebdev@gmail.com>
2017-12-04 15:03:14 -0200

    Finished almost everyting, expect JS DOM, Responsiveness and build

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,22 @@
+var gulp = require("gulp");
+var postCss = require("gulp-postcss");
+var autoPrefixer = require("autoprefixer");
+var cssVars = require("postcss-simple-vars");
+var cssNested = require("postcss-nested");
+var cssImport = require("postcss-import");
+var cssMixins = require("postcss-mixins");
+var cssHexRgba = require("postcss-hexrgba");
+
+
+
+
+gulp.task("styles", function() {
+    return gulp.src("./app/assets/styles/styles.css")
+    .pipe(postCss([cssImport, cssMixins, cssVars, cssNested, cssHexRgba, autoPrefixer]))
+    .on("error", function(errorInfo) {
+        console.log(errorInfo.toString());
+
+        this.emit("end");
+    })
+    .pipe(gulp.dest("./app/temp/styles"))
+});
